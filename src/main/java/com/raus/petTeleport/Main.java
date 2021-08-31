454871b9070d8e25a15c7527d09133aa8c586741
NotAShelf <62766066+NotAShelf@users.noreply.github.com>
2021-08-31 23:53:45 +0300

    add updatechecker & maven fixes

--- src/main/java/com/raus/petTeleport/Main.java
+++ src/main/java/com/raus/petTeleport/Main.java
@@ -1,14 +1,19 @@
 package com.raus.petTeleport;
 
+import de.jeff_media.updatechecker.UpdateChecker;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin
-{
+public class Main extends JavaPlugin {
+		private static final int SPIGOT_RESOURCE_ID = 77688;
+
 	@Override
-	public void onEnable()
-	{
+	public void onEnable(){
 		// Listeners
 		getServer().getPluginManager().registerEvents(new UnloadListener(), this);
+		UpdateChecker.init(this, SPIGOT_RESOURCE_ID)
+				.checkEveryXHours(24) // Check every 24 hours
+				.checkNow(); // And check right now
+
 	}
 
 	@Override
